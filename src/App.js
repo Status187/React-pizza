7d8408b821eb0c17687fc0d3b1de268b788df362
Maksim <status187@yandex.ru>
2022-04-22 10:03:14 +0300

    App

--- src/App.js
+++ src/App.js
@@ -3,55 +3,43 @@ import {Header} from './components'
 import {Home, Cart} from "./pages";
 import {Switch, Route, BrowserRouter} from 'react-router-dom'
 import axios from "axios";
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 // import store from "./redux/store";
 import {setPizzas} from "./redux/actions/pizzas";
 
 const CART_PAGE = "/cart";
 const HOME_PAGE = "/";
 
-// function App() {
-//     React.useEffect(() => {
-//         axios.get('http://localhost:3000/db.json').then(({data}) => {
-//             setPizzas(data.pizzas)
-//         });
-//     }, []);
-//
-//
-//
-//     return
-// }
+function App () {
+    const dispatch = useDispatch();
+    const hraniliche = useSelector(({pizzas, filters}) => {
+        return {
+            items: pizzas.items,
+            sortBy: filters.sortBy
+        }
+    });
 
-class App extends React.Component {
-    componentDidMount() {
+    console.log(hraniliche)
+
+    React.useEffect(() => {
         axios.get('http://localhost:3000/db.json').then(({data}) => {
-            window.store.dispatch(setPizzas(data.pizzas));
+            dispatch(setPizzas(data.pizzas));
         });
-    }
+    }, [])
 
-    render() {
-        console.log(this.props.items);
-        return(
-            <div className="wrapper">
-                <Header />
-                <div className="content">
-                    <BrowserRouter>
-                        <Switch>
-                            <Route path={CART_PAGE} component={Cart} exact/>
-                            <Route path={HOME_PAGE} render={() => <Home items={this.props.items}/>} exact/>
-
-                        </Switch>
-                    </BrowserRouter>
-                </div>
+    return(
+        <div className="wrapper">
+            <Header />
+            <div className="content">
+                <BrowserRouter>
+                    <Switch>
+                        <Route path={CART_PAGE} component={Cart} exact/>
+                        <Route path={HOME_PAGE} render={() => <Home items={[]}/>} exact/>
+                    </Switch>
+                </BrowserRouter>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
-const mapStateToProps = state => {
-    return {
-        items: state.pizzas.items
-    }
-}
-
-export default connect(mapStateToProps)(App);
+export default App;
